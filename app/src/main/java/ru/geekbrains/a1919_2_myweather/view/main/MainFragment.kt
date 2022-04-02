23f341e649b26a286bd0b52c1ff9c5f76b101c00
Nikolai <ndrovalev@gmail.com>
2022-04-02 08:51:29 +0300

    Урок 2. Создал новый проэкт.

---
+++ app/src/main/java/ru/geekbrains/a1919_2_myweather/view/main/MainFragment.kt
@@ -0,0 +1,33 @@
+package ru.geekbrains.a1919_2_myweather.view.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.geekbrains.a1919_2_myweather.R
+import ru.geekbrains.a1919_2_myweather.viewmodel.MainViewModel
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/ru/geekbrains/a1919_2_myweather/viewmodel/MainViewModel.kt
@@ -0,0 +1,7 @@
+package ru.geekbrains.a1919_2_myweather.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
